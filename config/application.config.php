23cbecac3d0be5cf4149e99761db54a797124721
Pedro Augusto <pedro@webferoz.com.br>
2014-05-07 00:53:59 -0300

    primeiro commit

---
+++ config/application.config.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+    'modules' => array(
+        'Application',
+		'DoctrineModule',
+    	'DoctrineORMModule',
+    	'Album'
+    ),
+    'module_listener_options' => array(
+        'module_paths' => array(
+            './module',
+            './vendor',
+        ),
+        'config_glob_paths' => array(
+            'config/autoload/{,*.}{global,local}.php',
+        ),
+    ),
+);
---
+++ module/Album/src/Album/Entity/Album.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Album\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Zend\InputFilter\InputFilter; // <- add isso
+use Zend\InputFilter\Factory as InputFactory; // <- add isso
+use Zend\InputFilter\InputFilterAwareInterface; // <- add isso
+use Zend\InputFilter\InputFilterInterface; // <- add isso
+
+/**
+ * Album
+ *
+ * @ORM\Table(name="album")
+ * @ORM\Entity
+ */
+class Album implements InputFilterAwareInterface // <- alterar isso "add implements InputFilterAwareInterface"
+{
+	protected $inputFilter; // <- add isso
+	
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="artist", type="string", length=100, nullable=false)
+     */
+    private $artist;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=100, nullable=false)
+     */
+    private $title;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set artist
+     *
+     * @param string $artist
+     * @return Album
+     */
+    public function setArtist($artist)
+    {
+        $this->artist = $artist;
+
+        return $this;
+    }
+
+    /**
+     * Get artist
+     *
+     * @return string 
+     */
+    public function getArtist()
+    {
+        return $this->artist;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Album
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    
+    /**
+     * Convert the object to an array.
+     *
+     * @return array
+     */
+    public function getArrayCopy()
+    {
+    	return get_object_vars($this);
+    }
+    
+    /**
+     * Populate from an array.
+     *
+     * @param array $data
+     */
+    public function populate($data = array())
+    {
+    	$this->id = $data['id'];
+    	$this->artist = $data['artist'];
+    	$this->title = $data['title'];
+    }
+    
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+    	throw new \Exception("Not used");
+    }
+    
+ 	// <- add isso tudo
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+ 
+            $factory = new InputFactory();
+ 
+            $inputFilter->add($factory->createInput(array(
+                'name'       => 'id',
+                'required'   => true,
+                'filters' => array(
+                    array('name'    => 'Int'),
+                ),
+            )));
+ 
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'artist',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            )));
+ 
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'title',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            )));
+ 
+            $this->inputFilter = $inputFilter;
+        }
+ 
+        return $this->inputFilter;
+    }
+}
